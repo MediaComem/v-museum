cf7b48fc6808b446f35cce5bd200b88492179238
Grégory Dozot <gregory.dozot@heig-vd.ch>
2021-07-16 14:25:01 +0200

    Add slider animation + add some tests

--- src/components/ImageSelector/ImageSelector.vue
+++ src/components/ImageSelector/ImageSelector.vue
@@ -1,6 +1,18 @@
 <template>
-  <!--el-row :class="{ customTransitionMask: step !== 3 }" :gutter="10" :align="'middle'"-->
-  <el-row :gutter="10" :align="'middle'">
+  <el-row
+    :gutter="10"
+    :align="'middle'"
+    :class="{
+      globalMaskFast: (step === 0 || step === 6) && zoomingStep === 3,
+      globalMaskMedium: (step === 1 || step === 2 || step === 4 || step === 5) && zoomingStep === 3,
+      removeGlobalMaskFastEnd: step === 3 && zoomingStep === 3 && (releaseStep === 0 || releaseStep === 6),
+      removeGlobalMaskFastToMedium: zoomingStep === 2 || zoomingStep === 1,
+      removeGlobalMaskMediumToNone: step === 3 && zoomingStep === 0,
+      removeGlobalMaskMediumEnd:
+        step === 3 && zoomingStep === 3 &&
+        (releaseStep === 1 || releaseStep === 2 || releaseStep === 4 || releaseStep === 5),
+    }"
+  >
     <el-col :xs="10" :sm="9" :md="8" :lg="8" :xl="8"> </el-col>
     <el-col
       :xs="3"
@@ -20,6 +32,16 @@
         :autoplay="false"
         ref="carousel"
         class="custom-carousel"
+        :class="{
+          zoomTransitionImageFast: (step === 0 || step === 6) && zoomingStep === 3,
+          zoomTransitionImageMedium: (step === 1 || step === 5) && zoomingStep === 3,
+          zoomTransitionImageSlow: (step === 2 || step === 4) && zoomingStep === 3,
+          unzoomTransitionImageSlow: step === 3 && ((releaseStep === 2 || releaseStep == 4) || (step === 3 && zoomingStep === 0)),
+          unzoomTransitionImageMedium: (step === 2 || step === 4) && zoomingStep === 1,
+          unzoomTransitionImageMediumEnd: step === 3 && zoomingStep === 3 && (releaseStep === 1 || releaseStep === 5),
+          unzoomTransitionImageFast: (step === 1 || step === 5) && zoomingStep === 2,
+          unzoomTransitionImageFastEnd: step === 3 && zoomingStep === 3 && (releaseStep === 0 || releaseStep === 6)
+        }"
         @change="changeImage"
       >
         <el-carousel-item v-for="(value, index) in data" :key="index">
@@ -35,6 +57,7 @@
     <el-col :xs="2" :sm="2" :md="2" :lg="2" :xl="2">
       <el-row :justify="'center'" :align="'middle'">
         <el-slider
+          ref="slider"
           vertical
           v-model="step"
           :height="windowHeight / 5 + 'px'"
@@ -72,6 +95,9 @@ export default {
     return {
       isInitialLoad: true,
       data: undefined,
+      releaseStep: -1,
+      previousStep: 3,
+      zoomingStep: 3,
       step: 3,
       windowHeight: undefined,
       windowWidth: undefined,
@@ -89,7 +115,9 @@ export default {
         this.getNextImages(this.currentSlide + 1, 80, 100);
       }
     },
-    rangeChange() {
+    rangeChange(releaseStep) {
+      this.zoomingStep = 3;
+      this.releaseStep = releaseStep;
       this.step = 3;
       this.stopTimeout();
     },
@@ -97,27 +125,62 @@ export default {
     changeImage(nextImageIndex) {
       switch (this.step) {
         case 0:
+          if (this.previousStep === 1){
+            this.zoomingStep = 3;
+          }
           this.navigateNextImage(nextImageIndex, 80, 100);
           break;
         case 1:
+          if (this.previousStep === 2){
+            this.zoomingStep = 3;
+          }
+          if (this.previousStep === 0) {
+            this.zoomingStep = 2;
+          }
           this.navigateNextImage(nextImageIndex, 40, 500);
           break;
         case 2:
+          if (this.previousStep === 3){
+            this.zoomingStep = 3;
+          }
+          if (this.previousStep === 1) {
+            this.zoomingStep = 1;
+          }
           this.navigateNextImage(nextImageIndex, 20, 1000);
           break;
         case 3:
+          if (this.previousStep === 4 || this.previousStep === 2) {
+            this.zoomingStep = 0;
+          }
           this.stopTimeout();
           break;
         case 4:
+          this.navigatePreviousImage(100);
+          if (this.previousStep === 3){
+            this.zoomingStep = 3;
+          }
+          if (this.previousStep === 5) {
+            this.zoomingStep = 1;
+          }
           this.navigatePreviousImage(1000);
           break;
         case 5:
+          if (this.previousStep === 4){
+            this.zoomingStep = 3;
+          }
+          if (this.previousStep === 6) {
+            this.zoomingStep = 2;
+          }
           this.navigatePreviousImage(500);
           break;
         case 6:
+          if (this.previousStep === 5){
+            this.zoomingStep = 3;
+          }
           this.navigatePreviousImage(100);
           break;
       }
+      this.previousStep = this.step;
     },
     navigatePreviousImage(intervalTransitionTime) {
       this.timeout = setTimeout(() => {
@@ -154,7 +217,7 @@ export default {
 
     this.carousel = this.$refs.carousel;
     //this.carousel.$el.addEventListener("wheel", this.mouseWheelAction);
-
+    
     // The parameter for the year search will come from the previous selection view.
     // Currently, this value is hard-coded for testing purpose.
     this.$store.dispatch("initializeCarousel", {
--- tests/unit/components/ImageSelector/image_selector.spec.js
+++ tests/unit/components/ImageSelector/image_selector.spec.js
@@ -37,13 +37,240 @@ describe("Test ImageSelector component", () => {
   });
 
   it("Test rangeChange method", () => {
+    wrapper.vm.$data.zoomingStep = 6;
     wrapper.vm.$data.step = 6;
+    expect(wrapper.vm.$data.releaseStep).to.equal(-1);
     expect(wrapper.vm.$data.step).to.equals(6);
-    wrapper.vm.rangeChange();
+    expect(wrapper.vm.$data.zoomingStep).to.equals(6);
+    wrapper.vm.rangeChange(2);
     expect(wrapper.vm.$data.step).to.equals(3);
+    expect(wrapper.vm.$data.zoomingStep).to.equals(3);
+    expect(wrapper.vm.$data.releaseStep).to.equal(2);
   });
 
-  it("Test getPreviousImages method without loading new content", () => {
-
+  it("Test changeImage call loading method", () => {
+    sandbox.stub(wrapper.vm, "loadNextImages").callsFake(() => {
+      return;
+    });
+    sandbox.stub(wrapper.vm, "stopTimeout").callsFake(() => {
+      return;
+    });
+    sandbox.stub(wrapper.vm, "loadPreviousImages").callsFake(() => {
+      return;
+    });
+    sandbox.spy(wrapper.vm.loadNextImages);
+    sandbox.spy(wrapper.vm.stopTimeout);
+    sandbox.spy(wrapper.vm.loadPreviousImages);
+    wrapper.vm.$data.step = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(0);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(0);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(0);
+    wrapper.vm.$data.step = 0;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(1);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(0);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(0);
+    wrapper.vm.$data.step = 1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(2);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(0);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(0);
+    wrapper.vm.$data.step = 2;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(3);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(0);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(0);
+    wrapper.vm.$data.step = 3;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(3);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(1);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(0);
+    wrapper.vm.$data.step = 4;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(3);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(1);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(1);
+    wrapper.vm.$data.step = 5;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(3);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(1);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(2);
+    wrapper.vm.$data.step = 6;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(3);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(1);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(3);
+    wrapper.vm.$data.step = 7;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.loadNextImages.callCount).to.equal(3);
+    expect(wrapper.vm.stopTimeout.callCount).to.equal(1);
+    expect(wrapper.vm.loadPreviousImages.callCount).to.equal(3);
   });
+
+  it("Test animation step management", () => {
+    sandbox.stub(wrapper.vm, "loadNextImages").callsFake(() => {
+      return;
+    });
+    sandbox.stub(wrapper.vm, "stopTimeout").callsFake(() => {
+      return;
+    });
+    sandbox.stub(wrapper.vm, "loadPreviousImages").callsFake(() => {
+      return;
+    });
+
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.$data.step = 0;
+    wrapper.vm.$data.previousStep = 1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(3);
+    expect(wrapper.vm.$data.previousStep).to.equal(0);
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.$data.previousStep = 0;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(-1);
+    expect(wrapper.vm.$data.previousStep).to.equal(0);
+    wrapper.vm.$data.step = 1;
+    wrapper.vm.$data.previousStep = 0;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(2);
+    expect(wrapper.vm.$data.previousStep).to.equal(1);
+    wrapper.vm.$data.previousStep = 2;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(3);
+    expect(wrapper.vm.$data.previousStep).to.equal(1);
+    wrapper.vm.$data.previousStep = 1;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(-1);
+    expect(wrapper.vm.$data.previousStep).to.equal(1);
+    wrapper.vm.$data.step = 2;
+    wrapper.vm.$data.previousStep = 2;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(-1);
+    expect(wrapper.vm.$data.previousStep).to.equal(2);
+    wrapper.vm.$data.previousStep = 1;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(1);
+    expect(wrapper.vm.$data.previousStep).to.equal(2);
+    wrapper.vm.$data.previousStep = 3;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(3);
+    expect(wrapper.vm.$data.previousStep).to.equal(2);
+    wrapper.vm.$data.step = 3;
+    wrapper.vm.$data.previousStep = 3;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(-1);
+    expect(wrapper.vm.$data.previousStep).to.equal(3);
+    wrapper.vm.$data.previousStep = 2;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(0);
+    expect(wrapper.vm.$data.previousStep).to.equal(3);
+    wrapper.vm.$data.previousStep = 4;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(0);
+    expect(wrapper.vm.$data.previousStep).to.equal(3);
+    wrapper.vm.$data.step = 4;
+    wrapper.vm.$data.previousStep = 4;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(-1);
+    expect(wrapper.vm.$data.previousStep).to.equal(4);
+    wrapper.vm.$data.previousStep = 3;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(3);
+    expect(wrapper.vm.$data.previousStep).to.equal(4);
+    wrapper.vm.$data.previousStep = 5;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(1);
+    expect(wrapper.vm.$data.previousStep).to.equal(4);
+    wrapper.vm.$data.step = 5;
+    wrapper.vm.$data.previousStep = 5;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(-1);
+    expect(wrapper.vm.$data.previousStep).to.equal(5);
+    wrapper.vm.$data.previousStep = 4;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(3);
+    expect(wrapper.vm.$data.previousStep).to.equal(5);
+    wrapper.vm.$data.previousStep = 6;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(2);
+    expect(wrapper.vm.$data.previousStep).to.equal(5);
+    wrapper.vm.$data.step = 6;
+    wrapper.vm.$data.previousStep = 6;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(-1);
+    expect(wrapper.vm.$data.previousStep).to.equal(6);
+    wrapper.vm.$data.previousStep = 5;
+    wrapper.vm.$data.zoomingStep = -1;
+    wrapper.vm.changeImage(4);
+    expect(wrapper.vm.$data.zoomingStep).to.equal(3);
+    expect(wrapper.vm.$data.previousStep).to.equal(6);
+  });
+
+  it("Test loadPreviousImages method", () => {
+    let carousel = {
+      prev: function() {},
+    };
+    wrapper.vm.$data.carousel = carousel;
+    sandbox.spy(wrapper.vm.$data.carousel);
+    wrapper.vm.$data.currentSlide = 0;
+    wrapper.vm.$data.isLoadingImage = true;
+    wrapper.vm.loadPreviousImages(10, 5, 0);
+    expect(wrapper.vm.$data.currentSlide).to.equal(10);
+    expect(wrapper.vm.$data.carousel.prev.callCount).to.equal(1);
+    expect($store.dispatch.callCount).to.equals(1);
+    wrapper.vm.$data.isLoadingImage = false;
+    wrapper.vm.loadPreviousImages(10, 5, 0);
+    expect(wrapper.vm.$data.currentSlide).to.equal(10);
+    expect(wrapper.vm.$data.carousel.prev.callCount).to.equal(2);
+    expect($store.dispatch.callCount).to.equals(1);
+    wrapper.vm.loadPreviousImages(10, 20, 0);
+    expect(wrapper.vm.$data.currentSlide).to.equal(10);
+    expect(wrapper.vm.$data.carousel.prev.callCount).to.equal(3);
+    expect($store.dispatch.callCount).to.equals(2);
+  });
+
+/* TODO: FIX TEST
+  it("Test loadNextImages method", () => {
+    let carousel = {
+      next: function() {},
+    };
+    wrapper.vm.$data.carousel = carousel;
+    sandbox.spy(wrapper.vm.$data.carousel);
+    wrapper.vm.$data.currentSlide = 0;
+    wrapper.vm.$data.totalCarouselIndex = 100;
+    wrapper.vm.$data.isLoadingImage = true;
+    wrapper.vm.loadNextImages(10, 95, 0);
+    expect(wrapper.vm.$data.currentSlide).to.equal(10);
+    expect(wrapper.vm.$data.carousel.next.callCount).to.equal(1);
+    expect($store.dispatch.callCount).to.equals(1);
+    
+    wrapper.vm.$data.isLoadingImage = false;
+    wrapper.vm.loadNextImages(10, 5, 0);
+    expect(wrapper.vm.$data.currentSlide).to.equal(10);
+    expect(wrapper.vm.$data.carousel.next.callCount).to.equal(2);
+    expect($store.dispatch.callCount).to.equals(1);
+
+    wrapper.vm.loadNextImages(10, 95, 0);
+    expect(wrapper.vm.$data.currentSlide).to.equal(10);
+    expect(wrapper.vm.$data.carousel.next.callCount).to.equal(3);
+    expect($store.dispatch.callCount).to.equals(2);
+  });
+  */
 });
