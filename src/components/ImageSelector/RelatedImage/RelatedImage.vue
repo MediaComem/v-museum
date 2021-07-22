2e21efaa009f25312333578b632dfd17f7a7b3ea
Grégory Dozot <gregory.dozot@heig-vd.ch>
2021-07-22 17:22:11 +0200

    Fix related image text position

--- src/components/ImageSelector/RelatedImage/RelatedImage.vue
+++ src/components/ImageSelector/RelatedImage/RelatedImage.vue
@@ -1,21 +1,43 @@
 <template>
   <div v-if="image && display" :style="{ height: imageHeight + 'px' }">
-    <p v-if="align === 'top' && justify === 'start'" class="relatedImageBase index-font start-text-align">
+    <p
+      v-if="align === 'top' && justify === 'start'"
+      class="relatedImageBase index-font start-text-align"
+      :style="componentWidthStart"
+    >
       {{ image.tag["@value"] }}
     </p>
-    <p v-if="align === 'top' && justify === 'end'" class="relatedImageBase index-font end-text-align">
+    <p
+      v-if="align === 'middle' && justify === 'start'"
+      class="relatedImageBase index-font start-text-align"
+    >
+      {{ image.tag["@value"] }}
+    </p>
+    <p
+      v-if="align === 'top' && justify === 'end'"
+      class="relatedImageBase index-font end-text-align"
+      :style="componentWidthEnd"
+    >
       {{ image.tag["@value"] }}
     </p>
     <img
       class="relatedImageBase"
       :src="image.result.imagePaths.large"
       :alt="image.result.id"
-      :style="{ height: imageHeight + 'px' }"
+      :style="{ height: (imageHeight / 10) * 9 + 'px', componentWidthEnd }"
     />
-    <p v-if="align === 'bottom' && justify === 'start'" class="relatedImageBase index-font start-text-align">
+    <p
+      v-if="align === 'bottom' && justify === 'start'"
+      class="relatedImageBase index-font start-text-align"
+      :style="componentWidthStart"
+    >
       {{ image.tag["@value"] }}
     </p>
-    <p v-if="align === 'bottom' && justify === 'end'" class="relatedImageBase index-font end-text-align">
+    <p
+      v-if="align === 'bottom' && justify === 'end'"
+      class="relatedImageBase index-font end-text-align"
+      :style="componentWidthEnd"
+    >
       {{ image.tag["@value"] }}
     </p>
   </div>
@@ -27,8 +49,17 @@ export default {
     image: Object,
     display: Boolean,
     imageHeight: Number,
+    imageWidth: Number,
     align: String,
     justify: String,
+  },
+  computed: {
+    componentWidthStart() {
+      return { paddingLeft: this.imageWidth / 6 + "px" };
+    },
+    componentWidthEnd() {
+      return { width: this.imageWidth + "px" };
+    },
   },
 };
 </script>
@@ -38,10 +69,10 @@ export default {
 @import "../relatedImageAnimation.css";
 
 .start-text-align {
-    text-align: start;
+  text-align: start;
 }
 
 .end-text-align {
-    text-align: end;
+  text-align: end;
 }
 </style>
