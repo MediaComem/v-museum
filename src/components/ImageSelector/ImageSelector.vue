78a92def76bcb8286ab95f3079ec0fad6bc7005a
Grégory Dozot <gregory.dozot@heig-vd.ch>
2021-08-03 10:16:00 +0200

    Prepare drag move + fix screen during related images display

--- src/components/ImageSelector/ImageSelector.vue
+++ src/components/ImageSelector/ImageSelector.vue
@@ -1,193 +1,201 @@
 <template>
-  <el-row :align="'bottom'">
-    <el-col :span="7">
-      <div ref="position1">
-        <related-image
-          :image="getRelatedPositionItem(1)"
-          :imageHeight="(windowHeight / 6) * 4"
-          :imageWidth="(windowWidth / 24) * 6"
-          :align="'bottom'"
-          :justify="'end'"
-        />
-      </div>
-    </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col :span="6"> </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col :span="7">
-      <div ref="position2">
-        <related-image
-          :image="getRelatedPositionItem(2)"
-          :imageHeight="(windowHeight / 6) * 4"
-          :imageWidth="(windowWidth / 24) * 6"
-          :align="'bottom'"
-          :justify="'start'"
-        />
-      </div>
-    </el-col>
-  </el-row>
-  <el-row>
-    <el-col :span="7"> </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col
-      :span="6"
-      :align="'bottom'"
-    >
-      <el-row>
-        <el-col :span="10">
-          <el-row>
-            <el-col :span="5">
-              <p>333</p>
-            </el-col>
-            <el-col :span="5">
-              <p class="year-font">1910's</p>
-            </el-col>
-          </el-row>
-        </el-col>
-        <el-col :span="14">
-          <el-row :justify="'end'">
-            <p
-              v-if="relatedImagesPosition.length >= 1 && step === 3"
-              class="index-font data-information"
-              :class="{
-                removeRelatedImageBaseText: relatedImagesPosition[0].display,
-              }"
-            >
-              {{ relatedImagesPosition[0].image.tag["@value"] }} &nbsp;
-            </p>
-            <p
-              v-if="relatedImagesPosition.length >= 2 && step === 3"
-              class="index-font data-information"
-              :class="{
-                removeRelatedImageBaseText: relatedImagesPosition[1].display,
-              }"
-            >
-              {{ relatedImagesPosition[1].image.tag["@value"] }} &nbsp;
-            </p>
-            <p
-              v-if="relatedImagesPosition.length >= 3 && step === 3"
-              class="index-font data-information"
-              :class="{
-                removeRelatedImageBaseText: relatedImagesPosition[2].display,
-              }"
-            >
-              {{ relatedImagesPosition[2].image.tag["@value"] }} &nbsp;
-            </p>
-            <p class="index-font data-information">
-              {{ currentIndex + 1 }}
-            </p>
-          </el-row>
-        </el-col>
-      </el-row>
-    </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col :span="7"> </el-col>
-  </el-row>
-  <el-row :gutter="10" :align="'middle'">
-    <el-col :span="7">
-      <div ref="position3">
-        <related-image
-          :image="getRelatedPositionItem(3)"
-          :imageHeight="(windowHeight / 6) * 4"
-          :imageWidth="(windowWidth / 24) * 6"
-          :align="'top'"
-          :justify="'end'"
-        />
-      </div>
-    </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col :span="6">
-      <div class="sliderMask">
-        <el-carousel
-          :indicator-position="'none'"
-          :height="'800px'"
-          direction="vertical"
-          :loop="false"
-          :autoplay="false"
-          ref="carousel"
-          class="custom-carousel"
-          :class="selectZoomAnimation"
-          @change="changeImage"
-        >
-          <el-carousel-item v-for="(value, index) in data" :key="index">
-            <img
-              ref="image"
-              class="custom-image"
-              :src="value.imagePaths.large"
-              :alt="value.id"
-            />
-          </el-carousel-item>
-        </el-carousel>
-      </div>
-    </el-col>
-    <el-col :span="2">
-      <el-row :justify="'center'" :align="'middle'">
-        <el-slider
-          ref="slider"
-          vertical
-          v-model="step"
-          :height="windowHeight / 5 + 'px'"
-          :max="6"
-          :show-tooltip="false"
-          @change="releaseSlider"
-          @input="changeImage"
-        >
-        </el-slider>
-      </el-row>
-    </el-col>
-    <el-col :span="7">
-      <div :style="{ marginLeft: windowWidth / 6 + 'px' }" ref="position4">
-        <related-image
-          :image="getRelatedPositionItem(4)"
-          :imageHeight="(windowHeight / 6) * 4"
-          :imageWidth="(windowWidth / 24) * 6"
-          :align="'middle'"
-          :justify="'start'"
-        />
-      </div>
-    </el-col>
-  </el-row>
-  <el-row>
-    <el-col :span="7"> </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col :span="6">
-      <el-row>
-        <p v-if="data" class="data-information">
-          {{ data[currentIndex].author }} <br />
-          {{ data[currentIndex].medium }}
-        </p>
-      </el-row>
-    </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col :span="7"> </el-col>
-  </el-row>
-  <el-row>
-    <el-col :span="7">
-      <div ref="position5">
-        <related-image
-          :image="getRelatedPositionItem(5)"
-          :imageHeight="(windowHeight / 6) * 4"
-          :imageWidth="(windowWidth / 24) * 6"
-          :align="'top'"
-          :justify="'end'"
-        />
-      </div>
-    </el-col>
-    <el-col :span="2"> </el-col>
-    <el-col :span="6"> </el-col>
-    <el-col :span="2"></el-col>
-    <el-col :span="7">
-      <div ref="position6">
-        <related-image
-          :image="getRelatedPositionItem(6)"
-          :imageHeight="(windowHeight / 6) * 4"
-          :imageWidth="(windowWidth / 24) * 6"
-          :align="'top'"
-          :justify="'start'"
-        />
-      </div>
-    </el-col>
-  </el-row>
+  <div
+    style="cursor: grab;"
+    @mousedown="startPosition"
+    @mouseup="endPosition"
+    @mousemove="mouseMove"
+    :style="[testTranslate]"
+    ref="display"
+  >
+    <!--div style="cursor: grab;" @click="startPosition" @mousemove="startPosition"-->
+    <el-row :align="'bottom'">
+      <el-col :span="7">
+        <div ref="position1">
+          <related-image
+            :image="getRelatedPositionItem(1)"
+            :imageHeight="(windowHeight / 6) * 4"
+            :imageWidth="(windowWidth / 24) * 6"
+            :align="'bottom'"
+            :justify="'end'"
+          />
+        </div>
+      </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="6"> </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="7">
+        <div ref="position2">
+          <related-image
+            :image="getRelatedPositionItem(2)"
+            :imageHeight="(windowHeight / 6) * 4"
+            :imageWidth="(windowWidth / 24) * 6"
+            :align="'bottom'"
+            :justify="'start'"
+          />
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="7"> </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="6" :align="'bottom'">
+        <el-row>
+          <el-col :span="10">
+            <el-row>
+              <el-col :span="5">
+                <p>333</p>
+              </el-col>
+              <el-col :span="5">
+                <p class="year-font">1910's</p>
+              </el-col>
+            </el-row>
+          </el-col>
+          <el-col :span="14">
+            <el-row :justify="'end'">
+              <p
+                v-if="relatedImagesPosition.length >= 1 && step === 3"
+                class="index-font data-information"
+                :class="{
+                  removeRelatedImageBaseText: relatedImagesPosition[0].display,
+                }"
+              >
+                {{ relatedImagesPosition[0].image.tag["@value"] }} &nbsp;
+              </p>
+              <p
+                v-if="relatedImagesPosition.length >= 2 && step === 3"
+                class="index-font data-information"
+                :class="{
+                  removeRelatedImageBaseText: relatedImagesPosition[1].display,
+                }"
+              >
+                {{ relatedImagesPosition[1].image.tag["@value"] }} &nbsp;
+              </p>
+              <p
+                v-if="relatedImagesPosition.length >= 3 && step === 3"
+                class="index-font data-information"
+                :class="{
+                  removeRelatedImageBaseText: relatedImagesPosition[2].display,
+                }"
+              >
+                {{ relatedImagesPosition[2].image.tag["@value"] }} &nbsp;
+              </p>
+              <p class="index-font data-information">
+                {{ currentIndex + 1 }}
+              </p>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="7"> </el-col>
+    </el-row>
+    <el-row :gutter="10" :align="'middle'">
+      <el-col :span="7">
+        <div ref="position3">
+          <related-image
+            :image="getRelatedPositionItem(3)"
+            :imageHeight="(windowHeight / 6) * 4"
+            :imageWidth="(windowWidth / 24) * 6"
+            :align="'top'"
+            :justify="'end'"
+          />
+        </div>
+      </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="6">
+        <div class="sliderMask">
+          <el-carousel
+            :indicator-position="'none'"
+            :height="'800px'"
+            direction="vertical"
+            :loop="false"
+            :autoplay="false"
+            ref="carousel"
+            class="custom-carousel"
+            :class="selectZoomAnimation"
+            @change="changeImage"
+          >
+            <el-carousel-item v-for="(value, index) in data" :key="index">
+              <img
+                ref="image"
+                class="custom-image"
+                :src="value.imagePaths.large"
+                :alt="value.id"
+              />
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-col>
+      <el-col :span="2">
+        <el-row :justify="'center'" :align="'middle'">
+          <el-slider
+            class="cursor-pointer"
+            ref="slider"
+            vertical
+            v-model="step"
+            :height="windowHeight / 5 + 'px'"
+            :max="6"
+            :show-tooltip="false"
+            @change="releaseSlider"
+            @input="changeImage"
+          >
+          </el-slider>
+        </el-row>
+      </el-col>
+      <el-col :span="7">
+        <div :style="{ marginLeft: windowWidth / 6 + 'px' }" ref="position4">
+          <related-image
+            :image="getRelatedPositionItem(4)"
+            :imageHeight="(windowHeight / 6) * 4"
+            :imageWidth="(windowWidth / 24) * 6"
+            :align="'middle'"
+            :justify="'start'"
+          />
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="7"> </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="6">
+        <el-row>
+          <p v-if="data" class="data-information">
+            {{ data[currentIndex].author }} <br />
+            {{ data[currentIndex].medium }}
+          </p>
+        </el-row>
+      </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="7"> </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="7">
+        <div ref="position5">
+          <related-image
+            :image="getRelatedPositionItem(5)"
+            :imageHeight="(windowHeight / 6) * 4"
+            :imageWidth="(windowWidth / 24) * 6"
+            :align="'top'"
+            :justify="'end'"
+          />
+        </div>
+      </el-col>
+      <el-col :span="2"> </el-col>
+      <el-col :span="6"> </el-col>
+      <el-col :span="2"></el-col>
+      <el-col :span="7">
+        <div ref="position6">
+          <related-image
+            :image="getRelatedPositionItem(6)"
+            :imageHeight="(windowHeight / 6) * 4"
+            :imageWidth="(windowWidth / 24) * 6"
+            :align="'top'"
+            :justify="'start'"
+          />
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -238,9 +246,31 @@ export default {
       currentSlide: 0,
 
       changeImageTimeout: undefined,
+      isDrag: false,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
+    startPosition() {
+      this.isDrag = true;
+    },
+    endPosition() {
+      this.isDrag = false;
+    },
+    mouseMove(event) {
+      if (this.isDrag) {
+        this.x = event.x;
+        this.y = event.y;
+      }
+    },
+    getTranslate() {
+      if (this.isDrag) {
+        return "translate(" + this.x + "," + this.y + ")";
+      } else {
+        return;
+      }
+    },
     mouseWheelAction(event) {
       if (event.deltaY < 0) {
         this.getPreviousImages(this.currentSlide - 1, 80, 100);
@@ -284,7 +314,7 @@ export default {
 
       // Stop loading of related images
       this.stopDisplayRelatedImages();
-      
+
       // Moving carousel part
       switch (this.step) {
         case 0:
@@ -370,13 +400,22 @@ export default {
         this.relatedImagesPosition.forEach((element, index) => {
           const animationDelay = 1000 + 2000 * index;
           this.displayRelatedImageTimeout.push(
-            setTimeout(
-              () => (this.relatedImagesPosition[index].display = true),
-              animationDelay
-            )
+            setTimeout(() => {
+              this.relatedImagesPosition[index].display = true;
+              this.$nextTick(() => {
+                this.setupSize(this.relatedImagesPosition[index].position);
+              });
+            }, animationDelay)
           );
         });
       });
+    },
+    setupSize(position) {
+      console.log(position);
+      if (position === 1 || position === 2) {
+        console.log("RESIZE");
+        window.scrollTo(0, (this.windowHeight / 6) * 4);
+      }
     },
   },
   computed: {
@@ -393,6 +432,11 @@ export default {
           this.step === 3 && (this.releaseStep === 0 || this.releaseStep === 6),
         unzoomTransitionImageFast:
           (this.step === 1 || this.step === 5) && this.zoomingStep === 1,
+      };
+    },
+    testTranslate() {
+      return {
+        transform: this.getTranslate(),
       };
     },
     ...mapState(["images", "isLoadingImage", "relatedImages"]),
@@ -423,4 +467,12 @@ export default {
 @import "./imageselector.css";
 @import "./carouselAnimation.css";
 @import "./relatedImageAnimation.css";
+
+.cursor-grab {
+  cursor: grab;
+}
+
+.cursor-pointer {
+  cursor: pointer;
+}
 </style>
