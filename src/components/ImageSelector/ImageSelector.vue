acfc89fe4994ab9e776e1bc8c2983652e6491683
Grégory Dozot <gregory.dozot@heig-vd.ch>
2021-10-14 13:24:16 +0200

    Fix position problem

--- src/components/ImageSelector/ImageSelector.vue
+++ src/components/ImageSelector/ImageSelector.vue
@@ -323,7 +323,7 @@
             relatedImagesPosition.length > 0 && relatedImagesPosition[0].image
           "
           class="image-information"
-          >
+        >
           <div
             :class="{
               removeRelatedImageBaseText:
@@ -331,14 +331,15 @@
                 !shouldRunRelatedImageTransition,
             }"
           >
-           {{ relatedImagesPosition[0].image.tag["@value"] }} &nbsp;
+            {{ relatedImagesPosition[0].image.tag["@value"] }} &nbsp;
           </div>
         </div>
         <div
           v-if="
             relatedImagesPosition.length > 1 && relatedImagesPosition[1].image
           "
-          class="image-information">
+          class="image-information"
+        >
           <div
             :class="{
               removeRelatedImageBaseText:
@@ -346,14 +347,15 @@
                 !shouldRunRelatedImageTransition,
             }"
           >
-           {{ relatedImagesPosition[1].image.tag["@value"] }} &nbsp;
+            {{ relatedImagesPosition[1].image.tag["@value"] }} &nbsp;
           </div>
         </div>
         <div
           v-if="
             relatedImagesPosition.length > 2 && relatedImagesPosition[2].image
           "
-          class="image-information">
+          class="image-information"
+        >
           <div
             :class="{
               removeRelatedImageBaseText:
@@ -545,32 +547,32 @@
     v-if="secondRelatedTagsElements.length > 0 && relatedOver"
     :style="secondImageInformationPosition"
   >
-    <div
-      style="overflow: hidden;"
-      :style="{ width: thumbnailWidth() + 'px' }"
-    >
+    <div style="overflow: hidden;" :style="{ width: thumbnailWidth() + 'px' }">
       <div
         v-if="secondRelatedTagsElements.length > 0"
         class="image-information"
       >
-        <div :class="{
-          removeRelatedImageBaseText:
-            secondRelatedImagesPosition.length > 0 &&
-            secondRelatedImagesPosition[0].display
-        }">
+        <div
+          :class="{
+            removeRelatedImageBaseText:
+              secondRelatedImagesPosition.length > 0 &&
+              secondRelatedImagesPosition[0].display,
+          }"
+        >
           {{ secondRelatedTagsElements[0]["@value"] }} &nbsp;
         </div>
       </div>
       <div
         v-if="secondRelatedTagsElements.length > 1"
         class="image-information"
-
       >
-        <div :class="{
-          removeRelatedImageBaseText:
-            secondRelatedImagesPosition.length > 0 &&
-            secondRelatedImagesPosition[1].display
-        }">
+        <div
+          :class="{
+            removeRelatedImageBaseText:
+              secondRelatedImagesPosition.length > 0 &&
+              secondRelatedImagesPosition[1].display,
+          }"
+        >
           {{ secondRelatedTagsElements[1]["@value"] }} &nbsp;
         </div>
       </div>
@@ -578,11 +580,13 @@
         v-if="secondRelatedTagsElements.length > 2"
         class="image-information"
       >
-        <div :class="{
+        <div
+          :class="{
             removeRelatedImageBaseText:
               secondRelatedImagesPosition.length > 0 &&
-              secondRelatedImagesPosition[2].display
-        }">
+              secondRelatedImagesPosition[2].display,
+          }"
+        >
           {{ secondRelatedTagsElements[2]["@value"] }} &nbsp;
         </div>
       </div>
@@ -1774,7 +1778,7 @@ export default {
         "user-select": "none",
       };
     },
-   imageInformationPositionTag() {
+    imageInformationPositionTag() {
       return {
         position: "absolute",
         top:
@@ -2372,6 +2376,11 @@ export default {
     }),
   },
   activated() {
+    if (this.currentXPosition <= 30 || this.currentYPosition <= 30) {
+      this.currentXPosition = this.defineLeftPositionCenterPage();
+      this.currentYPosition = this.defineTopPositionCenterPage();
+    }
+
     window.scrollTo({
       left: this.currentXPosition,
       top: this.currentYPosition,
