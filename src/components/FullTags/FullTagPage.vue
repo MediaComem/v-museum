9c7bb36bf40cb7e9dc6eb47c9d7edf8a27fbec07
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-01-17 17:36:41 +0100

    Change link from square image to medium image + adapt CSS for image object fit for FullTag view

--- src/components/FullTags/FullTagPage.vue
+++ src/components/FullTags/FullTagPage.vue
@@ -141,7 +141,7 @@ ul {
   .image-size {
     width: 150px;
     height: 150px;
-    object-fit: cover;
+    object-fit: fill;
   }
 }
 
@@ -157,7 +157,7 @@ ul {
   .image-size {
     width: 300px;
     height: 300px;
-    object-fit: cover;
+    object-fit: fill;
   }
 }
 
