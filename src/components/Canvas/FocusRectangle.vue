8f8f0dd3b4fd7bb4071e6e1527357d0d2ef7d50a
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-03-10 10:20:25 +0100

    Refactor Focus component

--- src/components/Canvas/FocusRectangle.vue
+++ src/components/Canvas/FocusRectangle.vue
@@ -1,64 +1,19 @@
 <template>
-  <svg
+  <div
     class="transition"
-    :width="imageWidth"
-    :height="imageHeight"
-    :style="{ top: topPosition, left: leftPosition }"
+    :style="{
+      position: 'fixed',
+      top: topPosition,
+      left: leftPosition,
+      height: imageHeight + 'px',
+      width: imageWidth + 'px',
+    }"
   >
-    <line x1="0" y1="0" x2="20" y2="0" stroke="black" stroke-width="2" />
-    <line x1="0" y1="0" x2="0" y2="20" stroke="black" stroke-width="2" />
-
-    <line
-      :x1="imageWidth - 20"
-      y1="0"
-      :x2="imageWidth"
-      y2="0"
-      stroke="black"
-      stroke-width="2"
-    />
-    <line
-      :x1="imageWidth"
-      y1="0"
-      :x2="imageWidth"
-      y2="20"
-      stroke="black"
-      stroke-width="2"
-    />
-
-    <line
-      x1="0"
-      :y1="imageHeight - 20"
-      x2="0"
-      :y2="imageHeight"
-      stroke="black"
-      stroke-width="2"
-    />
-    <line
-      x1="0"
-      :y1="imageHeight"
-      x2="20"
-      :y2="imageHeight"
-      stroke="black"
-      stroke-width="2"
-    />
-
-    <line
-      :x1="imageWidth"
-      :y1="imageHeight - 20"
-      :x2="imageWidth"
-      :y2="imageHeight"
-      stroke="black"
-      stroke-width="2"
-    />
-    <line
-      :x1="imageWidth - 20"
-      :y1="imageHeight"
-      :x2="imageWidth"
-      :y2="imageHeight"
-      stroke="black"
-      stroke-width="2"
-    />
-  </svg>
+    <div class="top left"></div>
+    <div class="top right"></div>
+    <div class="bottom right"></div>
+    <div class="bottom left"></div>
+  </div>
 </template>
 
 <script>
@@ -66,7 +21,7 @@ import {
   getImageHeight,
   getImageWidth,
   focusIntermediaryHeight,
-} from "./service/image_management_service";
+} from './service/image_management_service';
 export default {
   props: {
     offsetX: Number,
@@ -76,44 +31,75 @@ export default {
     imageFactor: Object,
     border: Boolean,
   },
-  name: "FocusRectangle",
+  name: 'FocusRectangle',
   computed: {
     topPosition() {
       let imageHeight = getImageHeight(this.focus, this.imageFactor.sizeFactor);
       if (!this.focus && this.hoverImage) {
         imageHeight = focusIntermediaryHeight(this.imageFactor.sizeFactor);
       }
-      return (this.offsetY - imageHeight) / 2 + "px";
+      return (this.offsetY - imageHeight) / 2 + 'px';
     },
     leftPosition() {
-      return (this.offsetX - getImageWidth(this.hoverImage, this.imageFactor.sizeFactor)) / 2 + "px";
+      return (
+        (this.offsetX -
+          getImageWidth(this.hoverImage, this.imageFactor.sizeFactor)) /
+          2 +
+        'px'
+      );
     },
     imageHeight() {
-      let imageHeight = getImageHeight(this.focus, this.imageFactor.sizeFactor) + 20;
+      let imageHeight =
+        getImageHeight(this.focus, this.imageFactor.sizeFactor) + 20;
       if (!this.focus && this.hoverImage) {
         imageHeight = focusIntermediaryHeight(this.imageFactor.sizeFactor);
       }
       return imageHeight;
     },
     imageWidth() {
-      return this.focus ? getImageWidth(this.focus, this.imageFactor.sizeFactor) + 20 : getImageWidth(this.hoverImage, this.imageFactor.sizeFactor) + 20;
+      return this.focus
+        ? getImageWidth(this.focus, this.imageFactor.sizeFactor) + 20
+        : getImageWidth(this.hoverImage, this.imageFactor.sizeFactor) + 20;
     },
     background() {
       return {
-        'background': this.focus ? 'rgba(0, 0, 0, 0.1)' : 'unset',
-      }
-    }
+        background: this.focus ? 'rgba(0, 0, 0, 0.1)' : 'unset',
+      };
+    },
   },
 };
 </script>
 
 <style scoped>
-svg {
-  position: fixed;
-  z-index: -11;
+.top,
+.bottom {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  pointer-events: none;
+}
+
+.top {
+  top: 0;
+  border-top: 1px solid;
+}
+
+.bottom {
+  bottom: 0;
+  border-bottom: 1px solid;
+}
+
+.left {
+  left: 0;
+  border-left: 1px solid;
+}
+
+.right {
+  right: 0;
+  border-right: 1px solid;
 }
 
 .transition {
-  transition: all 0.2s,
+  transition: all 0.2s;
 }
 </style>
