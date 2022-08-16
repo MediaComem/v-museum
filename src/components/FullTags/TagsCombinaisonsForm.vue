79c15937e5a242bd1fb1f1bffdcecbb82c7bc1a4
Timothée Dione <timothee.dione@gmail.com>
2022-08-16 09:51:50 +0200

    both with and without input managed in same computed property

--- src/components/FullTags/TagsCombinaisonsForm.vue
+++ src/components/FullTags/TagsCombinaisonsForm.vue
@@ -16,7 +16,9 @@
         <br />
         <span v-if="this.imagesWithTags.length > 0">{{ this.imagesWithTags.length }} documents</span>
         <span v-else>It seems no document contains the tags combinaison you entered.</span>
-        <span v-for="(tag, index) in tagsForSelection" :key="index" @click="addTag(tag.tag)" style="color:red">{{ tag.tag}}</span>
+        <!-- <span v-for="(tag, index) in tagsForSelection" :key="index" @click="addTag(tag.tag)" style="color:red">{{
+                tag.tag
+        }}</span> -->
     </div>
     <!-- <div class="removable-tags">
         <div v-for="(tag, index) in this.selected_tags key"
@@ -34,7 +36,8 @@ export default {
         return {
             selected_tags: [''],
             images: images,
-            no_input_tags_to_return: {},
+            selected_tags_with_images: {},
+            input_and_selected_tags_with_images: {},
             tags: tags,
         }
     },
@@ -50,23 +53,25 @@ export default {
                 const data_to_return = {
                     selected_tags: this.selected_tags,
                     images: this.imagesWithTags,
-                    tags_list: this.no_input_tags_to_return
+                    tags_list: this.selected_tags_with_images,
+                    input_filter_tags_list: this.input_and_selected_tags_with_images
                 }
                 console.log('passé en emit:', data_to_return)
                 this.$emit('updateTagsList', data_to_return)
             }
         },
-        arrayInArray(arr1, arr2) {
-            let i = 0
-            let ok = true 
+        userTagsInImageTags(arr1, arr2) {
+            //specify start_index = 1 so we dont take consideration of user input
+            let i = 1
+            let ok = true
             do {
-                if(!arr2.includes(arr1[i].toLowerCase())) {
+                if (!arr2.includes(arr1[i].toLowerCase())) {
                     ok = false
                 }
                 i++
             } while (ok && i < arr1.length)
             return ok
-        }
+        },
     },
     computed: {
         imagesWithTags() {
@@ -75,29 +80,44 @@ export default {
                 return this.images
             }
             //Reset tags with images for update
-            const tags_to_return = {}
-            //Get only images including tag in array of selected tags
+            const tags_without_input = {}
+            const tags_with_input = {}
+            //Get only images including all selected tags except first (user current input) in array of selected tags
             const images_with_tags = this.images.filter((im) => {
                 let ok = true
                 let i = 1;
+                //Check all selected tags except first (user current input) if in image tags
                 do {
-                //Management of case sensitivity for UX
+                    //Management of case sensitivity for UX
                     const img_lower_case_tags = []
                     im.tags.forEach(tg => {
                         img_lower_case_tags.push(tg.toLowerCase())
                     })
-                    //create a list of clickable tags with number of images
+                    // ensure current tag is in image tags
                     if (img_lower_case_tags.includes(this.selected_tags[i].toLowerCase())) {
+                        //create a list of clickable tags with number of images
                         img_lower_case_tags.forEach(img_tag => {
-                        //Ensure all selected tags are into the image tags    
-                        let all_tags_ok = this.arrayInArray(this.selected_tags, img_lower_case_tags)    
-                            // Cas 1, on a le tag dans l'image donc, get + add
-                            if (tags_to_return[img_tag] && all_tags_ok) {
-                                tags_to_return[img_tag].nb_images += 1
-                            } else if (!tags_to_return[img_tag] && all_tags_ok) {
-                            // Cas 2, on a pas le tag => ajouter tag + total = 1
-                                tags_to_return[img_tag] = {
-                                    nb_images: 1
+                            //Ensure all  confirmed selected tags are into the image tags before adding this image to the concerned tags possibilities.    
+                            let entered_tags_ok = this.userTagsInImageTags(this.selected_tags, img_lower_case_tags)
+                            // let entered_tags_ok = true
+                            // Case 1, this tag was already in a image to keep
+                            if (tags_without_input[img_tag] && entered_tags_ok) {
+                                tags_without_input[img_tag].nb_images += 1
+                                //Check if the possible tags match user unconfirmed input
+                                if (img_lower_case_tags.includes(this.selected_tags[0].toLowerCase())) {
+                                    if (tags_with_input[img_tag]) {
+                                        tags_with_input[img_tag].nb_images += 1
+                                    } else {
+                                        tags_with_input[img_tag] = { nb_images: 1 }
+                                    }
+                                }
+                            }
+                            // Case 2, no image parcoured had the tag to add yet
+                            else if (!tags_without_input[img_tag] && entered_tags_ok) {
+                                tags_without_input[img_tag] = { nb_images: 1 }
+                                //Check if possible tags matches user unconfirmed input
+                                if (img_lower_case_tags.includes(this.selected_tags[0].toLowerCase())) {
+                                    tags_with_input[img_tag] = { nb_images: 1 }
                                 }
                             }
                         })
@@ -106,32 +126,12 @@ export default {
                     }
                     i++;
                 } while (ok && i < this.selected_tags.length)
-                this.no_input_tags_to_return = tags_to_return
+                this.selected_tags_with_images = tags_without_input
+                this.input_and_selected_tags_with_images = tags_with_input
                 return ok
             })
-            console.log("LES IMAGES:", this.no_input_tags_to_return)
+            console.log("LES IMAGES:", this.selected_tags_with_images, "Les images + input:", this.input_and_selected_tags_with_images, "input: ", this.selected_tags[0])
             return images_with_tags
-        },
-        tagsForSelection() {
-            const tags_with_combi_and_string = []
-            this.tags.tags.forEach(tag => {
-                const formated_string = this.selected_tags[0].toLowerCase();
-                if (this.selected_tags.length == 1) {
-                    if(tag.toLowerCase().includes(formated_string)){
-                        const tag_data = {
-                        tag: tag.tag.toLowerCase(),
-                        nb_images: tag.totalImage
-                    } 
-                    }
-                } else {
-                    const stored_tag = this.no_input_tags_to_return[tag.tag.toLowerCase()]
-                    if (stored_tag != undefined) {
-                        tag_data.nb_images = stored_tag.nb_images
-                    }
-                    tags_with_string.push(tag_data)
-                }
-            })
-            return tags_with_combi_and_string
         },
     }
 }
